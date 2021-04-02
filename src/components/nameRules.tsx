3ef9065f311b95754da51564a4deb0e801d3436e
Jimmy Tang <Jimmy.Tang@quest.com>
2021-04-02 17:00:17 +0800

    remove action

--- src/components/nameRules.tsx
+++ src/components/nameRules.tsx
@@ -2,10 +2,11 @@ import React from "react";
 import {connect} from "react-redux";
 import {RootState} from "../redux/reducers";
 import {NamingRule, NamingRuleCollection, Rule} from "../shared/models/namingRuleCollection";
-import {NamingRuleAdd} from "../redux/actions/namingRuleAction";
+import {NamingRuleAdd, NamingRuleRemove} from "../redux/actions/namingRuleAction";
 interface Props {
     rules: NamingRuleCollection,
-    addRule: any
+    addRule: any,
+    removeRule: any
 }
 class NamingRulesComponentPure extends React.Component<Props>{
     add = () => {
@@ -14,9 +15,12 @@ class NamingRulesComponentPure extends React.Component<Props>{
         r.value.push({type: "FreeText", value: ''} as Rule);
         this.props.addRule(NamingRuleAdd(r));
     };
+    del = (item: NamingRule) => {
+        this.props.removeRule(NamingRuleRemove(item));
+    };
     render() {
         const rules = this.props.rules.getData() ? this.props.rules.getData().map((r, i) => {
-            return <li key={i}>{r.name}</li>
+            return <li key={i}>{r.name} <button onClick={() => this.del(r)}>delete</button></li>
         }) : '';
         return (<div>
             <ul>
@@ -34,9 +38,8 @@ const mapStoreToProps = (state: RootState ) => {
 };
 const mapDispatchToProps = (dispatch: any) => {
     return {
-        addRule: (action: any) => {
-            dispatch(action)
-        }
+        addRule: (action: any) => dispatch(action),
+        removeRule: (action: any) => dispatch(action)
     }
 };
 export const NamingRulesComponent = connect(mapStoreToProps, mapDispatchToProps)(NamingRulesComponentPure);
