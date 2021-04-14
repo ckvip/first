ede34315c4ae4bde43c69ea29e1a21f8ce4a0f35
Jimmy <59630466+ckvip@users.noreply.github.com>
2021-04-14 14:36:19 +0800

    update source code URL in about page (#16)
    * add Name preview and notification (#9)
    
    * Add About page
    
    * add tslint for react
    
    * Vue version (#11)
    
    * start vue version
    
    * finish vue version
    
    Co-authored-by: Jimmy Tang <Jimmy.Tang@quest.com>
    
    * update about and publish vue version (#13)
    
    update about page and publish vue version
    
    * Fix react link (#14)
    
    * fix router link issue for react version
    
    Co-authored-by: Jimmy Tang <Jimmy.Tang@quest.com>
    
    * update base url for react
    
    * update source code url
    
    Co-authored-by: Jimmy Tang <Jimmy.Tang@quest.com>

--- vue/src/views/About.vue
+++ vue/src/views/About.vue
@@ -9,10 +9,10 @@
       <td>React</td>
       <td>
         <p>
-          GitHub: <a href='https://github.com/ckvip/naming-rule/react' target='code'> https://github.com/ckvip/naming-rule/react</a>
+          GitHub: <a href='https://github.com/ckvip/naming-rule/tree/develop/react' target='code'> https://github.com/ckvip/naming-rule/react</a>
         </p>
         <p>
-          Gitee: <a href='https://gitee.com/yoso2020/naming-rule/react' target='code'> https://gitee.com/yoso2020/naming-rule/react</a>
+          Gitee: <a href='https://gitee.com/yoso2020/naming-rule/tree/develop/react' target='code'> https://gitee.com/yoso2020/naming-rule/react</a>
         </p>
       </td>
       <td>
@@ -28,10 +28,10 @@
       <td>Vue</td>
       <td>
         <p>
-          GitHub: <a href='https://github.com/ckvip/naming-rule/vue' target='code'> https://github.com/ckvip/naming-rule/vue</a>
+          GitHub: <a href='https://github.com/ckvip/naming-rule/tree/develop/vue' target='code'> https://github.com/ckvip/naming-rule/vue</a>
         </p>
         <p>
-          Gitee: <a href='https://gitee.com/yoso2020/naming-rule/vue' target='code'> https://gitee.com/yoso2020/naming-rule/vue</a>
+          Gitee: <a href='https://gitee.com/yoso2020/naming-rule/tree/develop/vue' target='code'> https://gitee.com/yoso2020/naming-rule/vue</a>
         </p>
       </td>
       <td>
